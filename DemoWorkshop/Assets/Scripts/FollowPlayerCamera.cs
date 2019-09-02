8f5285b9e84f3dc86f9fec2016dbfef285b396db
VALE-DESKTOP\Vale ZumZum <valeriaccolombo@gmail.com>
2019-09-01 22:31:03 -0300

    base code and scene

---
+++ DemoWorkshop/Assets/Scripts/FollowPlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float minXPosition = 0;
+    [SerializeField] private float minYPosition = 0;
+    [SerializeField] private float maxXPosition = 0;
+    [SerializeField] private float maxYPosition = 0;
+
+    private void Update()
+    {
+        Vector2 targetPosition = _target.position;
+        float xPosition = CheckLimits(targetPosition.x, minXPosition, maxXPosition);
+        float yPosition = CheckLimits(targetPosition.y, minYPosition, maxYPosition);
+        transform.position = new Vector3(xPosition, yPosition, -10);
+    }
+
+    private float CheckLimits(float targetPosition, float min, float max)
+    {
+        return Mathf.Max(min, Mathf.Min(max, targetPosition));
+    }
+}
--- DemoWorkshop/Assets/Scripts/Player.cs
+++ DemoWorkshop/Assets/Scripts/Player.cs
@@ -1,16 +1,113 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float RUN_SPEED = 1;
+    [SerializeField] private float JUMP_SPEED = 3;
+    
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D _rigidBody;
+
+    private bool _canPlay = true;
+    private bool _grounded = true;
+    private Vector3 _initialPosition;
+
+    private void Start()
     {
-        
+        _initialPosition = transform.localPosition;
     }
 
-    void Update()
+    private void Update()
     {
+        if (!_canPlay) return;
         
+        float speedX = Move();
+
+        Jump();
+        
+        SetAnimationTriggers(speedX);
+    }
+
+    private float Move()
+    {
+        float speedX = 0;
+        float speedY = _rigidBody.velocity.y;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            speedX = RUN_SPEED;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            speedX = -RUN_SPEED;
+        }
+
+        _rigidBody.velocity = new Vector2(speedX, speedY);
+        return speedX;
+    }
+
+    private void Jump()
+    {
+        if (_grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _grounded = false;
+            _rigidBody.AddForce(new Vector2(0, JUMP_SPEED), ForceMode2D.Impulse);
+        }
+    }
+
+    private void SetAnimationTriggers(float speedX)
+    {
+        _animator.SetFloat("velocityX", Mathf.Abs(speedX));
+        _animator.SetBool("grounded", _grounded);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.transform.tag)
+        {
+            case "Ground":
+                _grounded = true;
+                break;
+            case "Death":
+                Die();
+                break;
+            case "Enemy":
+                Die();
+                break;
+            case "Collectible":
+                break;
+            case "LevelTarget":
+                Win();
+                break;
+        }
+    }
+
+    private void Win()
+    {
+        _canPlay = false;
+        _animator.SetTrigger("victory");
+    }    
+
+    private void Die()
+    {
+        _canPlay = false;
+        _animator.SetBool("dead", true);
+        _animator.SetTrigger("hurt");
+
+        StartCoroutine(RespawnInAWhile());
+    }
+
+    private IEnumerator RespawnInAWhile()
+    {
+        yield return new WaitForSeconds(2);
+        
+        transform.localPosition = _initialPosition;
+        _animator.SetBool("dead", false);
+
+        yield return new WaitForSeconds(1);
+        _canPlay = true;
     }
 }
